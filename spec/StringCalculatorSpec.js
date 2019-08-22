c14b03110c7cb96c29ccb1e54a0248fd1dcbc743
DeadMan-MOC <mokgohloallie@gmail.com>
2019-08-22 16:24:53 +0200

    Tests

--- spec/StringCalculatorSpec.js
+++ spec/StringCalculatorSpec.js
@@ -1,7 +1,22 @@
-describe("Password validator", ()=>{
-    it ("Checks for strong password.", ()=> {
+describe("Calculator for strings", ()=>{
+    it ("Checks if it can add intergers in the string", ()=> {
         var raw = "5,1,0,1a\n3";
         var answer = Add(raw)
         expect(answer).toBe(10);
+    })
+    it ("Checks if it can add intergers in the string", ()=> {
+        var raw = "5,//,1a\n3";
+        var answer = Add(raw)
+        expect(answer).toBe(8);
+    })
+    it ("Checks if it can add intergers in the string", ()=> {
+        var raw = "5,1,0,1a\n3,-1";
+        var answer = Add(raw)
+        expect(answer).toBe(9);
+    })
+    it ("Checks if it can add intergers in the string", ()=> {
+        var raw = "-5,1,0,1a\n3";
+        var answer = Add(raw)
+        expect(answer).toBe(0);
     })
 })
