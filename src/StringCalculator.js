4c304d751ab752e85b182c3a8b472331e5432b71
DeadMan-MOC <mokgohloallie@gmail.com>
2019-08-22 10:59:54 +0200

    --- Auto-Git Commit ---

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,3 +1,21 @@
 function Add(array) {
-   
+    var sum = 0;
+    
+
+    for (let i in array) {
+        if (isNaN(array[i])) {
+            let x = String.array[i];
+            x = split.array[i];
+
+            for(i < x.length; i++;) {
+                if (x[i]){
+                    
+                }
+            }
+        }
+        else {
+            sum = sum + parseInt(array[i]);
+        }  
+    }
+    return sum;
 }
