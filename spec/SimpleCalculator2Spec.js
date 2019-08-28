216a2e591bfaa47951aeb186850fd53a6e9e418e
amokgohl <mokgohloallie@gmail.com>
2019-08-28 17:01:52 +0200

    not done

--- spec/SimpleCalculator2Spec.js
+++ spec/SimpleCalculator2Spec.js
@@ -1,23 +1,27 @@
 describe("Calculator for strings", ()=>{
-    Calculator = new Calculator;
-    it ("Checks if it can add intergers in the string", ()=> {
-        var raw = "5,1,0,1,3";
-        var answer = Calculator.Add(raw)
-        expect(answer).toBe(10);
+    
+    it ("Sum of multiple integers", ()=> {
+        const calculate = new Calculator;
+        var raw = (-1, -1);
+        var answer = calculate.Add(raw)
+        expect(answer).toBe();
+    })
+    it ("Sum of 2 integers", ()=> {
+        const calculate = new Calculator;
+        var raw = [5,3];
+        var answer = Add(raw)
+        expect(answer).toBe(8);
     })
     it ("Checks if it can add intergers in the string", ()=> {
-        var raw = "5,1,3";
-        var answer = Add(raw)
+        const calculate = new Calculator;
+        var raw = [5,1,0,1,3,-1];
+        var answer = calculate.Add(raw)
         expect(answer).toBe(9);
     })
-    it ("Checks if it can add intergers in the string", ()=> {
-        var raw = "5,1,0,1,3,-1";
-        var answer = Add(raw)
-        expect(answer).toBe(9);
-    })
-    it ("Checks if it can add intergers in the string", ()=> {
-        var raw = "-5,1,0,1,3";
-        var answer = Add(raw)
+    it ("Add multiple numbers", ()=> {
+        const calculate = new Calculator;
+        var raw = [-5,1,0,1,3];
+        var answer = calculate.Add(raw)
         expect(answer).toBe(0);
     })
 })
--- src/SimpleCalculator2.js
+++ src/SimpleCalculator2.js
@@ -2,32 +2,43 @@ class Calculator{
   constructor(){
      this.array = [];
   }
-  Add(){
-      let sum = 0;
-      for(let i = 0; i < arguments.length; i++){
-          sum += parseFloat(arguments[i]);
-          this.Last = sum;
-  }
-  return sum;
-  }
+  // Add(){
+  //     let sum = 0;
+  //     for(let i = 0; i < arguments.length; i++){
+  //         sum += arguments[i]
+  //         this.Last = sum;
+  // }
+  // return sum;
+  // }
+
+  Add() {
+    var sum = 0;
+    var arr = [];
+    for (let i in arr) {
+      sum = sum + arr[i];
+      this.Last = sum;
+    }
+    return sum;
+}
   Multiply(){
-      let sum = 1;
-      for(let i = 0; i < arguments.length; i++){
-          sum *= parseInt(arguments[i]);
-          this.Last = sum;
-      }
-      return sum;
+    var sum = 1;
+    var arr = [];
+    for (let i in arr) {
+      sum = sum * arr[i];
+      this.Last = sum;
+    }
+    return sum;
   }
-  Last(){
-      return this.Last;
-  }
-  set_slot(number){
-      this.array.push(this.Last);
-      return this.array[number - 1];
-  }
-  get_slot(number){
-      return this.array[number - 1];
-  }
+  // Last(){
+  //     return this.Last;
+  // }
+  // set_slot(number){
+  //     this.array.push(this.Last);
+  //     return this.array[number - 1];
+  // }
+  // get_slot(number){
+  //     return this.array[number - 1];
+  // }
  }
 
- let Calculator = new calculator;
+ let calculate = new Calculator;
