5e816bf3dcc4766fce2ebb7a5849201e3a593b29
DeadMan-MOC <mokgohloallie@gmail.com>
2019-09-10 16:20:43 +0200

    string calculator

--- spec/StringCalculatorSpec.js
+++ spec/StringCalculatorSpec.js
@@ -1,23 +1,38 @@
 describe("Calculator for strings", ()=>{
-    
-    it ("Sum of multiple integers", ()=> {
-        var raw = (-1, -1);
-        var answer = calculate.Add(raw)
-        expect(answer).toBe();
+
+    it ("Sum of 2 numbers", ()=> {
+        var raw = (1,2);
+        var answer = Add(raw)
+        expect(answer).toBe(3);
     })
-    it ("Sum of 2 integers", ()=> {
+
+    it ("Sum of 2 numbers", ()=> {
         var raw = (5,3);
         var answer = Add(raw)
         expect(answer).toBe(8);
     })
+    
     it ("Checks if it can add intergers in the string", ()=> {
-        var raw = [5,1,0,1,3,-1];
+        var raw = (5,1,0,1,3,-1);
         var answer = calculate.Add(raw)
-        expect(answer).toBe(9);
+        expect(answer).toThrow(new Error("Negative numbers not allowed"));
     })
-    it ("Add multiple numbers", ()=> {
-        var raw = (-5,1,0,1,3);
+    
+    it ("Checks if it can add intergers with delimiters", ()=> {
+        var raw = ("1,\n");
         var answer = calculate.Add(raw)
-        expect(answer).toBe(0);
+        expect(answer).toThrow(new Error("Error cannot be added"));
+    })
+    
+    it ("Sum of multiple numbers with delimiters", ()=> {
+        var raw = ("1\n2,3");
+        var answer = calculate.Add(raw)
+        expect(answer).toBe(6);
+    })
+
+    it ("Checks if it can add intergers with delimiters", ()=> {
+        var raw = ("//;\n1;2");
+        var answer = calculate.Add(raw)
+        expect(answer).toBe(3);
     })
 })
--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,1 +1,19 @@
-function String => ()
+function Add(str){
+    let regex = /[/;-\n]/gm;
+    if(str.includes("-")){
+        throw new Error("negatives not allowed.")
+    };
+   arguments = str.match(regex)
+    if(str == "") {
+        return 0;    
+    };
+    let sum = 0;
+    for(let i = 0; i < arguments.length; i++){
+            if(numbers[i] >= 1001){
+                continue;                    
+            } else{
+                sum = sum + parseInt(arguments[i])
+            };
+        };
+    return sum;
+};
