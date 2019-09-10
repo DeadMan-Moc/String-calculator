a9cbb6f1e9693a12b5e934d0911fd3c409019539
Alfred Mokgohloa <45788338+DeadMan-Moc@users.noreply.github.com>
2019-09-10 16:55:10 +0200

    Update StringCalculator.js

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,19 +1,36 @@
-// function Add(str){
-//     let regex = /[/;-\n]/gm;
-//     if(str.includes("-")){
-//         throw new Error("negatives not allowed.")
-//     };
-//    arguments = str.match(regex)
-//     if(str == "") {
-//         return 0;    
-//     };
-//     let sum = 0;
-//     for(let i = 0; i < arguments.length; i++){
-//             if(numbers[i] >= 1001){
-//                 continue;                    
-//             } else{
-//                 sum = sum + parseInt(arguments[i])
-//             };
-//         };
-//     return sum;
-// };
+ var Add = function(string) {
+
+    //splits given string with the regex.
+    //
+    var digits = function() {
+      var array = [];
+      var str2Splitted;
+      str2Splitted = string.split(/[^\d|^-]/);
+      for (i = 0; i < str2Splitted.length; i++) {
+        array.push(Number(str2Splitted[i]));
+      }
+      return array;
+    }();
+
+
+    //checks for negativesmin the array.
+    //if found it pushes it to array
+    var Neg = function() {
+      var array = [];
+
+      for(var  i = 0; i < digits.length; i++) {
+        if (digits[i] < 0) {
+          array.push(digits[i]);
+        }
+      }
+
+      return array;
+    }();
+
+    //adds all digits in the array to sum
+    var sum = 0;
+    for (var i = 0; i < digits.length; i++) {
+      sum = sum + digits[i];
+    }
+    return sum;
+  }
