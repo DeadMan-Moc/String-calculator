072ac4f07bb60824887f6de684896ff0ae3ef2f3
DeadMan-MOC <mokgohloallie@gmail.com>
2019-09-10 13:35:01 +0200

    --- Auto-Git Commit ---

--- spec/StringCalculatorSpec.js
+++ spec/StringCalculatorSpec.js
@@ -1,26 +1,22 @@
 describe("Calculator for strings", ()=>{
     
     it ("Sum of multiple integers", ()=> {
-        const calculate = new Calculator;
         var raw = (-1, -1);
         var answer = calculate.Add(raw)
         expect(answer).toBe();
     })
     it ("Sum of 2 integers", ()=> {
-        const calculate = new Calculator;
-        var raw = [5,3];
+        var raw = (5,3);
         var answer = Add(raw)
         expect(answer).toBe(8);
     })
     it ("Checks if it can add intergers in the string", ()=> {
-        const calculate = new Calculator;
         var raw = [5,1,0,1,3,-1];
         var answer = calculate.Add(raw)
         expect(answer).toBe(9);
     })
     it ("Add multiple numbers", ()=> {
-        const calculate = new Calculator;
-        var raw = [-5,1,0,1,3];
+        var raw = (-5,1,0,1,3);
         var answer = calculate.Add(raw)
         expect(answer).toBe(0);
     })
