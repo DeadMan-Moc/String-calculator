1f77ea1e5250bcae56f3e0a95051c9d1d45f56c5
Alfred Mokgohloa <45788338+DeadMan-Moc@users.noreply.github.com>
2019-09-10 16:22:10 +0200

    Update StringCalculator.js

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,19 +1,19 @@
-function Add(str){
-    let regex = /[/;-\n]/gm;
-    if(str.includes("-")){
-        throw new Error("negatives not allowed.")
-    };
-   arguments = str.match(regex)
-    if(str == "") {
-        return 0;    
-    };
-    let sum = 0;
-    for(let i = 0; i < arguments.length; i++){
-            if(numbers[i] >= 1001){
-                continue;                    
-            } else{
-                sum = sum + parseInt(arguments[i])
-            };
-        };
-    return sum;
-};
+// function Add(str){
+//     let regex = /[/;-\n]/gm;
+//     if(str.includes("-")){
+//         throw new Error("negatives not allowed.")
+//     };
+//    arguments = str.match(regex)
+//     if(str == "") {
+//         return 0;    
+//     };
+//     let sum = 0;
+//     for(let i = 0; i < arguments.length; i++){
+//             if(numbers[i] >= 1001){
+//                 continue;                    
+//             } else{
+//                 sum = sum + parseInt(arguments[i])
+//             };
+//         };
+//     return sum;
+// };
